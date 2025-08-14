36d1009f3bbaef31bee5a6f95bedfab56ba93a83
Cedrixian05 <158244660+Cedrixian05@users.noreply.github.com>
2025-08-14 09:32:18 +0800

    Add files via upload

---
+++ class_management_server.js
@@ -0,0 +1,48 @@
+const express = require(`express`);
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+let users = [
+    { id: 1, lastName: `Nocum`, firstName: `cedrix`, section: `BSIT 4A`, status: `p` },
+    { id: 2, lastName: `Nocum1`, firstName: `cedrix1`, section: `BSIT 4A`, status: `a` }
+];
+
+app.post(`/users`, (req, res) => {
+    const { lastName, firstName, section, status } = req.body;
+
+    const userIndex = users.findIndex(user => user.firstName === firstName && user.lastName === lastName);
+
+    if (userIndex !== -1) {
+        users[userIndex].status = status;
+        console.log(`Updated attendance for ${lastName} ${firstName} to ${status}`);
+        res.status(200).json({ message: `Attendance for ${lastName} ${firstName} has been updated to ${status}.` });
+    } else {
+        const newUser = {
+            id: users.length + 1,
+            lastName,
+            firstName,
+            section,
+            status
+        };
+
+        users.push(newUser);
+        console.log(`New user added: ${lastName} ${firstName} with status: ${status}`);
+        res.status(201).json({ message: `New student ${lastName} ${firstName} has been added with status: ${status}` });
+    }
+});
+
+app.get(`/users`, (req, res) => {
+    res.status(200).json(users);
+});
+
+app.get(`/status`, (req, res) => {
+    res.send(`Server is up and running!`);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
+
+module.exports = app;
